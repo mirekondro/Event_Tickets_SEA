c7ec50f5cf74f2fc1f3173a47670290bbe219a37
sasahrad <sasahrad123@gmail.com>
2026-04-08 10:00:48 +0200

    Events
    The app now shows events in the admin window

--- src/main/java/dk/easv/event_tickets_sea/gui/AdminController.java
+++ src/main/java/dk/easv/event_tickets_sea/gui/AdminController.java
@@ -1,13 +1,16 @@
 package dk.easv.event_tickets_sea.gui;
 
 import dk.easv.event_tickets_sea.HelloApplication;
+import dk.easv.event_tickets_sea.model.Event;
 import dk.easv.event_tickets_sea.model.User;
+import dk.easv.event_tickets_sea.util.EventManager;
 import dk.easv.event_tickets_sea.util.UserManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -22,15 +25,26 @@ public class AdminController {
     @FXML private TableColumn<User, String> colRole;
     @FXML private TableColumn<User, String> colEmail;
 
+    @FXML private TableView<Event> eventsTable;
+    @FXML private TableColumn<Event, String> colEventName;
+    @FXML private TableColumn<Event, String> colCoordinator;
+
     @FXML
     public void initialize() {
-        // Setup table columns
+        // Setup user table columns
         colUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
         colRole.setCellValueFactory(new PropertyValueFactory<>("role"));
         colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
 
         // Load users into table
         usersTable.setItems(UserManager.getInstance().getUsers());
+
+        // Setup events table columns
+        colEventName.setCellValueFactory(new PropertyValueFactory<>("eventName"));
+        colCoordinator.setCellValueFactory(new PropertyValueFactory<>("coordinator"));
+
+        // Load events into table
+        eventsTable.setItems(EventManager.getInstance().getEvents());
     }
 
     @FXML
@@ -49,5 +63,43 @@ public class AdminController {
         stage.setTitle("Add New User");
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.showAndWait(); // Changed to showAndWait so table refreshes after closing
+    }
+
+    @FXML
+    private void handleDeleteEvent() {
+        Event selectedEvent = eventsTable.getSelectionModel().getSelectedItem();
+        if (selectedEvent != null) {
+            EventManager.getInstance().removeEvent(selectedEvent);
+            showAlert(Alert.AlertType.INFORMATION, "Success",
+                    "Event Deleted",
+                    "Event '" + selectedEvent.getEventName() + "' has been deleted.");
+        } else {
+            showAlert(Alert.AlertType.WARNING, "No Selection",
+                    "No Event Selected",
+                    "Please select an event to delete.");
+        }
+    }
+
+    @FXML
+    private void handleAssignCoordinator() {
+        Event selectedEvent = eventsTable.getSelectionModel().getSelectedItem();
+        if (selectedEvent != null) {
+            // TODO: Open dialog to assign coordinator
+            showAlert(Alert.AlertType.INFORMATION, "Feature Coming Soon",
+                    "Assign Coordinator",
+                    "This feature will allow you to assign coordinators to events.");
+        } else {
+            showAlert(Alert.AlertType.WARNING, "No Selection",
+                    "No Event Selected",
+                    "Please select an event to assign a coordinator.");
+        }
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String header, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
